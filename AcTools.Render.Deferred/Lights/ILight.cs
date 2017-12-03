ecd6e2e8635a8f9dd0897744d0d9a5fb8965529a
Surfin Bird <illvdg13@gmail.com>
2017-12-03 03:17:59 +0300

    Update

---
+++ AcTools.Render.Deferred/Lights/ILight.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using AcTools.Render.Base;
+using AcTools.Render.Base.Cameras;
+
+namespace AcTools.Render.Deferred.Lights {
+    public interface ILight : IDisposable {
+        void Draw(DeviceContextHolder holder, ICamera camera, SpecialLightMode mode);
+    }
+}
---
+++ AcTools.Render.Deferred/Lights/SpecialLightMode.cs
@@ -0,0 +1,5 @@
+﻿namespace AcTools.Render.Deferred.Lights {
+    public enum SpecialLightMode {
+        Default, Shadows, ShadowsWithoutFilter, Debug
+    }
+}
